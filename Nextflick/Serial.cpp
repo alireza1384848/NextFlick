2a99818087837b1201b3a26a145aa9dcf882af60
Ehsankarimzadeh <160233596+Ehsankarimzadeh@users.noreply.github.com>
2025-01-22 12:10:13 +0330

    FixVideoAndSerialsClass

--- Nextflick/Serial.cpp
+++ Nextflick/Serial.cpp
@@ -1,1 +1,20 @@
 #include "Serial.h"
+
+Serial::Serial
+(std::string name, int year, double time, Country country,
+	Genre genre, Language language, double score,
+	std::string story, int countOfSeasons, int countOfParts) :
+	Video(name, year, time, country, genre, language, score, story)
+	, countOfParts(countOfParts), countOfSeasons(countOfSeasons)
+{
+}
+
+int Serial::getcountOfSeasons()
+{
+	return countOfSeasons;
+}
+
+int Serial::getcountOfParts()
+{
+	return countOfParts;
+}
